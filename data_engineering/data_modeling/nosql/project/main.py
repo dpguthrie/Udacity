511ec71654b26687214f85ede4388514270135a1
dpguthrie <douglas.p.guthrie@gmail.com>
2019-12-04 11:10:29 -0700

    Update functions utilized in entrypoint
    - Use argument from command line in function to get file directory
    - Further define dataframe dtype conversion to integers
    - Notify user of insert completion following each table

--- data_engineering/data_modeling/nosql/project/main.py
+++ data_engineering/data_modeling/nosql/project/main.py
@@ -1,18 +1,23 @@
 import pandas as pd
 import os
 import glob
+from config import sparkify_dictionary
+from cql import ETL
 import argparse
 
-file_directory = os.getcwd()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--filepath", help="Filepath containing csv files")
-args = parser.parse_args()
+def get_file_directory(filepath):
+    """File directory where data files reside
 
-if args.filepath:
-    file_directory += args.filepath
-else:
-    file_directory += '/event_data'
+    Arguments:
+        filepath {str}
+
+    Returns:
+        str -- Absolute path of directory where data files reside
+    """
+    cwd = os.getcwd()
+    cwd += filepath
+    return cwd
 
 
 def get_files(file_directory, format="csv"):
@@ -30,5 +35,36 @@ def construct_dataframe_from_files(files):
     df = pd.concat((pd.read_csv(f) for f in files))
     df.dropna(subset=['artist'], inplace=True)
     df[['itemInSession', 'sessionId', 'userId']] = \
-        df[['itemInSession', 'sessionId', 'userId']].apply(pd.to_numeric)
+        df[['itemInSession', 'sessionId', 'userId']].apply(
+            pd.to_numeric, downcast='integer')
     return df
+
+
+def run(filepath):
+    """Entrypoint into ETL script
+
+    Steps:
+    - Retrieve files
+    - Construct a dataframe from files
+    - Initialize ETL class with dictionary and dataframe
+    - For each key in dictionary, create table and insert data corresponding
+      to respective data in dataframe
+
+    Arguments:
+        filepath {str} -- Folder where files reside
+    """
+    file_directory = get_file_directory(filepath)
+    files = get_files(file_directory)
+    dataframe = construct_dataframe_from_files(files)
+    etl = ETL(sparkify_dictionary, dataframe)
+    for key in etl.dictionary.keys():
+        etl.run(key)
+        print(f"Data for {etl._table(key)} inserted")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filepath", help="Filepath where files reside")
+    args = parser.parse_args()
+    filepath = args.filepath if args.filepath else '/event_data'
+    run(filepath)
